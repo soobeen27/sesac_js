71d010ef2c3d8a93486a31468be9792c0712522c
Jang Soobeen <youdew66@gmail.com>
2025-11-18 17:39:17 +0900

    [study] css login 페이지 만들기

--- 1.HTML/5.JS/script.js
+++ 1.HTML/5.JS/script.js
@@ -1,6 +1,5 @@
-
 console.log("Hello JS");
 function sayHello() {
-    h1 = document.getElementById("title");
-    h1.style.color = "red";
+  h1 = document.getElementById("title");
+  h1.style.color = "red";
 }
