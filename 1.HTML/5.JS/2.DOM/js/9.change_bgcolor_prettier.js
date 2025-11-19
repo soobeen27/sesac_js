c3870797346fecc1ad3a201f5496164a9fa5b5f3
Jang Soobeen <youdew66@gmail.com>
2025-11-19 21:13:18 +0900

    [assignment-refactor] 글자, 배경색 js에서 처리하던거 css로 이관

--- 1.HTML/5.JS/2.DOM/js/9.change_bgcolor_prettier.js
+++ 1.HTML/5.JS/2.DOM/js/9.change_bgcolor_prettier.js
@@ -1,5 +1,3 @@
-let rgb = "";
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -10,40 +8,8 @@ function getRandomRGB() {
     )})`;
 }
 
-function darkerColor(rgb, level) {
-    return `rgb(${rgb
-        .match(/\d+/g)
-        .map((strNum) => {
-            return strNum - level > 0 ? strNum - level : 0;
-        })
-        .join(", ")})`;
-}
-
-function setDarkerElementsBG() {
-    const darkerRgb = darkerColor(rgb, 70);
-    const elements = document.getElementsByClassName("coloredBG");
-
-    Array.from(elements).forEach((element) => {
-        element.style.backgroundColor = darkerRgb;
-    });
-}
-
-function setDarkerFont() {
-    const darkerRgb = darkerColor(rgb, 30);
-    const elements = document.getElementsByClassName("coloredFont");
-
-    Array.from(elements).forEach((element) => {
-        element.style.color = darkerRgb;
-    });
-}
-
-// 다 하고 알았는데 굳이 어두운 rgb안만들고 그냥 background-color랑 color 어느정도 어두운색하고
-// 투명도 주면 되는거 아닌가..?
-
 function changeBGColorRandom() {
-    rgb = getRandomRGB();
+    const rgb = getRandomRGB();
     document.body.style.backgroundColor = rgb;
     document.getElementById("rgbSpan").textContent = rgb;
-    setDarkerElementsBG();
-    setDarkerFont();
 }
