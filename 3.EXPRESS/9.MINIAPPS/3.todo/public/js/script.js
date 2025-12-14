ff2a446e4b74a93afa71b8a3cd448be2e842cbed
Jang Soobeen <youdew66@gmail.com>
2025-12-14 15:19:10 +0900

    [assignment] todo server

--- 3.EXPRESS/9.MINIAPPS/3.todo/public/js/script.js
+++ 3.EXPRESS/9.MINIAPPS/3.todo/public/js/script.js
@@ -1,3 +1,25 @@
+const getTodos = (callback) => {
+    fetch('/api/todo')
+        .then((res) => {
+            if (!res.ok) throw new Error('get: todo/api 실패');
+            return res.json();
+        })
+        .then((data) => {
+            callback(data);
+        })
+        .catch((e) => console.log(e));
+};
+
+const drawTodos = (data) => {
+    const todoList = document.querySelector('#todo-list');
+    data.forEach((el) => {
+        const li = document.createElement('li');
+        li.innerText = el.message;
+        console.log(typeof el.message);
+        todoList.appendChild(li);
+    });
+};
 document.addEventListener('DOMContentLoaded', () => {
     console.log('dom loading done');
+    getTodos(drawTodos);
 });
--- 3.EXPRESS/9.MINIAPPS/3.todo/server.js
+++ 3.EXPRESS/9.MINIAPPS/3.todo/server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 let todos = [];
+let id = 0;
 
 app.use(express.static('public'));
 app.use(express.json()); //fe 에서 보낸 데이터가 json이면 파싱에서 req.body에 담음
@@ -13,24 +14,32 @@ app.use(morgan('dev'));
 
 app.get('/api/todo', (req, res) => {
     console.log('todo 조회 요청');
-    res.json(todo);
+    res.json(todos);
 });
 
 app.post('/api/todo', (req, res) => {
     console.log('todo 생성 요청');
     console.log(JSON.stringify(req.body));
-    const newTodo = { id: crypto.randomUUID(), todo: req.body.todo };
+    const newTodo = { id: id++, message: req.body.message, done: req.body.done };
     console.log(newTodo);
     todos.push(newTodo);
     res.json(newTodo);
 });
 
-app.put('/api/todo', (req, res) => {
+app.put('/api/todo/:id', (req, res) => {
     console.log('todo 수정 요청');
+    const id = parseInt(req.params.id);
+    const index = todos.findIndex((el) => el.id === id);
+    todos[index].done = !todos[index].done;
+    res.json({ message: 'put successed' });
 });
 
-app.delete('/api/todo', (req, res) => {
+app.delete('/api/todo/:id', (req, res) => {
     console.log('todo 삭제요청');
+    const id = parseInt(req.params.id);
+    const index = todos.findIndex((el) => el.id === id);
+    todos.splice(index, 1);
+    res.json({ message: 'delete successed' });
 });
 
 app.listen(PORT, () => {
