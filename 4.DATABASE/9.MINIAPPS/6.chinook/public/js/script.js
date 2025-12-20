ef122769270e716921eb5888b8aa4cdf83f5be10
Jang Soobeen <youdew66@gmail.com>
2025-12-20 18:15:54 +0900

    [assignment] chinook 열개 가져와서 잘 그리는지 확인

--- 4.DATABASE/9.MINIAPPS/6.chinook/public/js/script.js
+++ 4.DATABASE/9.MINIAPPS/6.chinook/public/js/script.js
@@ -1,5 +1,31 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const res = await fetch('/api/tracks?limit=10&offset=0');
+const createLi = (name) => {
+    const li = document.createElement('li');
+    li.innerText = name;
+    return li;
+};
+
+const drawLi = (lis) => {
+    const ulSearchResult = document.querySelector('#ul-search-result');
+    ulSearchResult.innerHTML = '';
+    lis.forEach((li) => {
+        ulSearchResult.appendChild(li);
+    });
+};
+
+const fetchTracks = async (limit, offset) => {
+    const res = await fetch(`/api/tracks?limit=${limit}&offset=${offset}`);
     const data = await res.json();
-    console.log(data);
+    const count = data.shift();
+    return { count, data };
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const { count, data } = await fetchTracks(10, 0);
+    console.log(count);
+    const lis = data.map((track) => {
+        return createLi(track['Name']);
+    });
+    drawLi(lis);
 });
+
+// TODO: 페이지네이션 하기
